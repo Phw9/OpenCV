2bc577ab871eae3205a9a4ef349d816818c51d8c
Phw9 <hyunwoo1417@hanyang.ac.kr>
2021-10-11 20:18:00 +0900

    Stitching

--- Panorama.cpp
+++ Panorama.cpp
@@ -4,12 +4,17 @@
 using namespace cv;
 using namespace std;
 
+// 명령행 인자에 영상 3개 입력이 안되면 오류 발생
+
 int main(int argc, char* argv[]) {
 
 	if (argc < 3) {
 		cerr << "Usage: stitching.exe <image_file1> <image_file2> [<image_file3> ...]" << endl;
 		return -1;
 	}
+
+	// vector<Mat> imgs 변수에 인자로 전달된 영상 입력.
+	// 영상파일이 비어있으면 오류 발생
 
 	vector<Mat> imgs;
 	for (int i = 1; i < argc ; i++) {
@@ -22,6 +27,8 @@ int main(int argc, char* argv[]) {
 		imgs.push_back(img);
 	}
 
+	// stitcher 객체를 생성 후 영상을 붙인다.
+
 	Ptr<Stitcher> stitcher = Stitcher::create();
 
 	Mat dst;
@@ -31,6 +38,7 @@ int main(int argc, char* argv[]) {
 		cerr << "Error on stitching!" << endl;
 		return -1;
 	}
+	// 파노라마 영상을 result.jpg로 저장한다.
 
 	imwrite("result.jpg", dst);
 
