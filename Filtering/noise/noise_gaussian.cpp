82408e4aaa545376614858459e039bd51974a270
Phw9 <hyunwoo1417@hanyang.ac.kr>
2022-04-21 09:01:21 +0900

    noise

---
+++ Filtering/noise/noise_gaussian.cpp
@@ -0,0 +1,43 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+
+using namespace std;
+using namespace cv;
+
+void noise_gaussian() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed" << endl;
+		return;
+	}
+
+	imshow("src", src);
+
+	for (int stddev = 10; stddev <= 30; stddev += 10) {
+		Mat noise(src.size(), CV_32SC1);
+		randn(noise, 0, stddev);
+
+		Mat dst;
+		add(src, noise, dst, Mat(), CV_8U);
+
+		String desc = format("stddev = %d", stddev);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(100), 1, LINE_AA);
+		imshow("dst", dst);
+		waitKey();
+	}
+	
+	destroyAllWindows();
+
+}
+
+
+
+
+int main(void) {
+
+	noise_gaussian();
+
+	return 0;
+}
